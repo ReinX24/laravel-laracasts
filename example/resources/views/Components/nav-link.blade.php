51fa9e4f0ecc8e196e6d7595162d6b2106bd1fb9
Rein <reinX244@gmail.com>
2024-11-26 08:21:54 +0800

    Added finished nav link components for home, about, and contact.

---
+++ example/resources/views/Components/nav-link.blade.php
@@ -0,0 +1,11 @@
+<!-- Converts active attribute into a variable -->
+@props(['active' => false, 'type' => 'a'])
+
+<!-- To run php scripts in our applicationm, we use the @/php and @/endphp tags -->
+
+<{{ $type == 'a' ? 'a' : 'button' }}
+    class="{{ $active ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white" }} rounded-md px-3 py-2 text-sm font-medium"
+    aria-current="{{ $active ? 'page' : 'false' }}"
+    {{ $attributes }}>
+    {{ $slot }}
+</{{  $type == 'a' ? 'a' : 'button' }}>
