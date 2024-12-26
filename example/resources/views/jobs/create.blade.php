07709244e448b5175b60721ae2394053f3d10e65
Rein <reinX244@gmail.com>
2024-12-26 23:59:02 +0800

    Started extracting components for jobs pages.

--- example/resources/views/jobs/create.blade.php
+++ example/resources/views/jobs/create.blade.php
@@ -13,25 +13,19 @@
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
+                        <x-form-label for="title">Title</x-form-label>
                         <div class="mt-2">
-                            <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input type="text" name="title" id="title" class="block min-w1 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder="Shift Leader" required>
-                            </div>
-                        </div>
+                            <x-form-input name="title" id="title" placeholder="CEO" />
 
-                        @error("title")
-                        <p class="text-xs text-red-500 font-semibold mt-1">
-                            {{ $message }}
-                        </p>
-                        @enderror
+                            <x-form-error name="title" />
+                        </div>
                     </div>
 
                     <div class="sm:col-span-4">
                         <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
                         <div class="mt-2">
                             <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input type="text" name="salary" id="salary" class="block min-w2 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder="$50,000 Per Year" required>
+                                <input type="text" name="salary" id="salary" class="block min-w2 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder="$50,000 Per Year">
                             </div>
                         </div>
 
@@ -41,7 +35,6 @@
                         </p>
                         @enderror
                     </div>
-
                 </div>
 
                 <!-- <div class="mt-10">
