3cb75ea5072502858d71aa27392a0aa7bebed8e9
Rein <reinX244@gmail.com>
2025-01-03 00:08:16 +0800

    Learned how to use JobPosted class.

--- example/routes/web.php
+++ example/routes/web.php
@@ -3,10 +3,19 @@
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\SessionController;
+use App\Mail\JobPosted;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 // Dummy route
-Route::get('/test', function () {});
+Route::get('/test', function () {
+    // return new JobPosted();
+    Mail::to("test@example.com")->send(
+        new JobPosted()
+    );
+
+    return "Done";
+});
 
 Route::view("/", "home", [
     "greeting" => "Hello",
